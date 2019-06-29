a1b5b7af98fbe9cde115f8d15accb1f731aeaa75
mohshbool <mohammad.alshbool2002@gmail.com>
2019-06-29 03:30:30 +0300

    remove testing code

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@
 
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
-import fireabse from 'react-native-firebase';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,10 +19,6 @@ const instructions = Platform.select({
 type Props = {};
 export default class App extends Component<Props> {
   render() {
-    fireabse
-      .database()
-      .ref('users/GZtGjXzdkjhIFM7KeGkvnWDgTiV2')
-      .once('value', s => console.log(s.val()));
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Hello!</Text>
