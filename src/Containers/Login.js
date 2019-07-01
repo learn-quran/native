88b100375829d75f96f9c494f1dfb06f14327f67
mohshbool <mohammad.alshbool2002@gmail.com>
2019-07-01 15:50:07 +0300

    finish functionality

--- src/Containers/Login.js
+++ src/Containers/Login.js
@@ -1,13 +1,13 @@
 // @flow
 import * as React from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import { Alert, View, StyleSheet, Dimensions } from 'react-native';
 import * as Yup from 'yup';
 import { Formik } from 'formik';
 import { TextInput, Button } from 'react-native-paper';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-//import { DropDown } from '../Components';
+import { DropDown } from '../Components';
+import { withFirebase } from '../Firebase';
 
-// eslint-disable-next-line no-unused-vars
 const LoginSchema = Yup.object().shape({
   password: Yup.string().required('password-is-required'),
   email: Yup.string()
@@ -36,11 +36,28 @@ class Login extends React.Component<Props, State> {
     };
   }
 
-  // eslint-disable-next-line no-unused-vars
   submit = (values: Object) => {
     const changeIsSubmitting = isSubmitting => this.setState({ isSubmitting });
     changeIsSubmitting(true);
-    changeIsSubmitting(false);
+    LoginSchema.validate(values, {
+      strict: true,
+      stripUnknown: true,
+    })
+      .then(() => {
+        this.props.firebase
+          .signIn(values)
+          .then(() => {})
+          .catch(error => {
+            Alert.alert(error);
+            changeIsSubmitting(false);
+          });
+      })
+      .catch(({ message }) => {
+        if (message !== 'NO_MESSAGE') {
+          DropDown.error(message);
+        }
+        changeIsSubmitting(false);
+      });
   };
 
   render() {
@@ -131,4 +148,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Login;
+export default withFirebase(Login);
