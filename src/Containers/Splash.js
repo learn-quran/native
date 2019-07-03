7aab1036110f5b20ae28df2b7ee44965f7017102
mohshbool <mohammad.alshbool2002@gmail.com>
2019-07-04 01:21:49 +0300

    match language with db

--- src/Containers/Splash.js
+++ src/Containers/Splash.js
@@ -1,19 +1,62 @@
 import React from 'react';
 import LottieView from 'lottie-react-native';
-import firebase from 'react-native-firebase';
+import RNRestart from 'react-native-restart';
+import { I18nManager } from 'react-native';
+import { connect } from 'react-redux';
+import { changeLanguage } from '../Actions';
+import { withFirebase } from '../Firebase';
+import { withTranslation } from 'react-i18next';
 
 type Props = {
   navigation: Object,
+  firebase: Object,
+  i18n: Object,
+  configs: Object,
+  changeLanguage: typeof changeLanguage,
 };
 class Login extends React.Component<Props> {
   constructor(props) {
     super(props);
-    this.auth();
+    setTimeout(() => this.auth(), 50);
   }
 
   auth = () => {
+    const { firebase, navigation, i18n, configs, changeLanguage } = this.props;
     firebase.auth().onAuthStateChanged(user => {
-      this.props.navigation.navigate(user ? 'SignedIn' : 'SignedOut');
+      if (user) {
+        firebase.getUser().then(({ language }) => {
+          if (configs.language !== language || i18n.language !== language) {
+            changeLanguage(language);
+            i18n.changeLanguage(language).then(() => {
+              I18nManager.forceRTL(language === 'ar');
+              I18nManager.allowRTL(language === 'ar');
+              if (
+                (language === 'ar' && !I18nManager.isRTL) ||
+                (language === 'en' && I18nManager.isRTL)
+              ) {
+                setTimeout(() => {
+                  RNRestart.Restart();
+                }, 0);
+              }
+            });
+          }
+        });
+      } else {
+        const {
+          i18n: { language },
+        } = this.props;
+        if (
+          (language === 'ar' && !I18nManager.isRTL) ||
+          (language === 'en' && I18nManager.isRTL)
+        ) {
+          I18nManager.forceRTL(language === 'ar');
+          I18nManager.allowRTL(language === 'ar');
+          setTimeout(() => {
+            RNRestart.Restart();
+          }, 0);
+        }
+      }
+      navigation.navigate(user ? 'SignedIn' : 'SignedOut');
     });
   };
 
@@ -24,4 +67,10 @@ class Login extends React.Component<Props> {
   }
 }
 
-export default Login;
+const mapStateToProps = ({ configs }) => ({ configs });
+const mapDispatchToProps = { changeLanguage };
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withTranslation()(withFirebase(Login)));
