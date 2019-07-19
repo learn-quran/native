bc16cd269244828b280e3d7e47bf656b123a57e5
mohshbool <mohammad.alshbool2002@gmail.com>
2019-07-19 20:25:17 +0300

    added Account page

---
+++ src/Containers/Account.js
@@ -0,0 +1,175 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { withTranslation } from 'react-i18next';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { DropDown } from '../Components';
+import { withFirebase } from '../Firebase';
+import { InputCard } from '../Components';
+
+type Props = {
+  firebase: Object,
+  t: Function,
+};
+type State = {};
+class Account extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSubmitting: false,
+      user: false,
+    };
+  }
+
+  componentDidMount() {
+    this.persistUserInfo();
+  }
+
+  persistUserInfo = () => {
+    const { firebase, t } = this.props;
+    firebase
+      .getUser()
+      .then(user => {
+        this.setState({ user: { ...user } });
+      })
+      .catch(() => {
+        DropDown.error(
+          t('something-went-wrong-please-close-the-tab-and-try-again'),
+        );
+      });
+  };
+
+  onUsernameSubmit = username => {
+    const { firebase, t } = this.props;
+    if (username.length >= 3) {
+      if (username !== this.state.user.username) {
+        if (/^(?:[\u0600-\u065f]+|[a-z]+)$/i.test(username)) {
+          firebase
+            .isUsernameDuplicated(username)
+            .then(() => {
+              this.setState({ isSubmitting: true });
+              firebase
+                .updateUserOnDB({ username })
+                .then(() =>
+                  DropDown.success(t('your-username-has-been-updated')),
+                )
+                .catch(err => DropDown.error(t(err)))
+                .finally(() => {
+                  this.setState({ isSubmitting: false });
+                  this.persistUserInfo();
+                });
+            })
+            .catch(err => DropDown.error(t(err)));
+        } else DropDown.error(t('username-can-only-contain-letters'));
+      }
+    } else DropDown.error(t('username-too-short'));
+  };
+  onEmailSubmit = email => {
+    const { firebase, t } = this.props;
+    if (
+      // eslint-disable-next-line
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i.test(email)
+    ) {
+      if (email !== this.state.user.email) {
+        this.setState({ isSubmitting: true });
+        firebase
+          .updateUserEmail(email)
+          .then(() => DropDown.success(t('your-email-has-been-updated')))
+          .catch(err => DropDown.error(t(err)))
+          .finally(() => {
+            this.setState({ isSubmitting: false });
+            this.persistUserInfo();
+          });
+      }
+    } else DropDown.error('Invalid email');
+  };
+  onPasswordSubmit = password => {
+    const { firebase, t } = this.props;
+    if (password.length > 1) {
+      if (password.length > 6) {
+        this.setState({ isSubmitting: true });
+        firebase
+          .updateUserPassword(password)
+          .then(() => {
+            DropDown.success(t('password-updated-successfully'));
+          })
+          .catch(err => DropDown.error(t(err)))
+          .finally(() => {
+            this.setState({ isSubmitting: false });
+          });
+      } else DropDown.error(t('password-is-too-short'));
+    }
+  };
+  onReauthSubmit = password => {
+    const { firebase, t } = this.props;
+    if (password.length > 1 && password.length > 6) {
+      this.setState({ isSubmitting: true });
+      firebase
+        .reauthenticate(password)
+        .then(() => {
+          DropDown.success(t('account-reauthenticated-successfully'));
+        })
+        .catch(err => DropDown.error(t(err)))
+        .finally(() => {
+          this.setState({ isSubmitting: false });
+        });
+    }
+  };
+
+  render() {
+    const { state, props } = this;
+    const { t } = props;
+    return (
+      <KeyboardAwareScrollView>
+        {!!this.state.user && (
+          <View style={styles.container}>
+            <InputCard
+              label={t('username')}
+              inheritedValue={state.user.username}
+              onSubmit={this.onUsernameSubmit}
+              buttonText={t('change-username')}
+              isSubmitting={state.isSubmitting}
+            />
+            <InputCard
+              label={t('email')}
+              inheritedValue={state.user.email}
+              onSubmit={this.onEmailSubmit}
+              buttonText={t('change-email')}
+              isSubmitting={state.isSubmitting}
+              keyboardType="email-address"
+            />
+            <InputCard
+              secureTextEntry
+              label={t('password')}
+              helperText={t(
+                'enter-your-new-password-below-you-need-to-be-recently-logged-in-to-change-your-password',
+              )}
+              onSubmit={this.onPasswordSubmit}
+              buttonText={t('change-password')}
+              isSubmitting={state.isSubmitting}
+            />
+            <InputCard
+              secureTextEntry
+              label={t('password')}
+              helperText={t(
+                'enter-your-password-below-to-re-authenticate-your-account',
+              )}
+              onSubmit={this.onReauthSubmit}
+              buttonText={t('re-authenticate')}
+              isSubmitting={state.isSubmitting}
+            />
+          </View>
+        )}
+      </KeyboardAwareScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+});
+
+export default withTranslation()(withFirebase(Account));
--- src/Containers/index.js
+++ src/Containers/index.js
@@ -4,5 +4,6 @@ import Home from './Home';
 import Splash from './Splash';
 import Root from './Root';
 import ForgotPassword from './ForgotPassword';
+import Account from './Account';
 
-export { Login, Signup, Home, Splash, Root, ForgotPassword };
+export { Login, Signup, Home, Splash, Root, ForgotPassword, Account };
