48cfee4acf37bedab9ae0d538d90e4fa7b2b488c
mohshbool <alshboolmoh@gmail.com>
2019-07-31 00:46:47 +0300

    add settings page and remove home

--- src/Containers/index.js
+++ src/Containers/index.js
@@ -1,6 +1,6 @@
 export { default as Login } from './Login';
 export { default as Signup } from './Signup';
-export { default as Home } from './Home';
+export { default as Settings } from './Settings';
 export { default as Splash } from './Splash';
 export { default as Root } from './Root';
 export { default as ForgotPassword } from './ForgotPassword';
--- src/Navigation/SignedIn.js
+++ src/Navigation/SignedIn.js
@@ -3,30 +3,30 @@ import React from 'react';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import { Home } from '../Containers';
+import { Settings } from '../Containers';
 
 /// This throws an error if imported from the index in src/Containers -- I HAVE NO IDEA WHY
 import Account from '../Containers/Account';
 
 const navigator = createMaterialBottomTabNavigator(
   {
-    Home: {
-      screen: Home,
+    Settings: {
+      screen: Settings,
     },
     Account: {
       screen: Account,
     },
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Settings',
     shifting: true,
     defaultNavigationOptions: ({ navigation }) => ({
       tabBarIcon: () => {
         const { routeName } = navigation.state;
         let iconName;
         switch (routeName) {
-          case 'Home':
-            iconName = 'home';
+          case 'Settings':
+            iconName = 'settings';
             break;
           case 'Account':
             iconName = 'account';
